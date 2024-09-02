6dc9b77abf60af4d3a32f80bcf1835e6a78f1337
vishali.k <vishalikesavan77@gmail.com>
2024-09-02 11:00:26 +0530

    added

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo/api")
+
+public class DemoController {
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome";
+	}
+
+}
